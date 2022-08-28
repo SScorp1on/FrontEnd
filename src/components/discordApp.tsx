52757db439a85cbad65cfdb07b1ec05bc1cc54bf
Jourloy <jourloy@yandex.ru>
2022-08-28 03:05:29 +0300

    ✏️ Update

--- src/components/discordApp.tsx
+++ src/components/discordApp.tsx
@@ -6,15 +6,16 @@ import {
 	Table,
 	ActionIcon,
 	Pagination,
+	Group,
 } from "@mantine/core";
 import {useDocumentTitle} from "@mantine/hooks";
 import {Menu} from "tabler-icons-react";
 
 const elements = [
-	{owner: `Jourloy`, name: `Carbon`},
-	{owner: `SScorp1on`, name: `Nitrogen`},
-	{owner: `Miaytt`, name: `Yttrium`},
-	{owner: `BasedType`, name: `Barium`},
+	{owner: ``, name: ``},
+	{owner: ``, name: ``},
+	{owner: ``, name: ``},
+	{owner: ``, name: ``},
 ];
 
 const rows = elements.map(element => (
@@ -34,25 +35,50 @@ export default function DiscordApp() {
 
 	return (
 		<>
-			<Paper shadow="sm" radius="md" p="md" withBorder style={{maxWidth: `550px`}}>
-				<Center>
-					<Title order={3}>Голосовые каналы</Title>
-				</Center>
-				<Table striped>
-					<thead>
-						<tr>
-							<th>Название</th>
-							<th>Владелец</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>{rows}</tbody>
-				</Table>
-				<Space h="sm" />
-				<Center>
-					<Pagination size="sm" total={3} radius="md" withControls={false} />
-				</Center>
-			</Paper>
-		</>
+			<Group grow>
+				<Paper shadow="sm" radius="md" p="md" withBorder style={{maxWidth: `550px`}}>
+					<Center>
+						<Title order={3}>Голосовые каналы</Title>
+					</Center>
+					<Table striped>
+						<thead>
+							<tr>
+								<th>Название</th>
+								<th>Владелец</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>{rows}</tbody>
+					</Table>
+					<Space h="sm" />
+					<Center>
+						<Pagination size="sm" total={3} radius="md" withControls={false} />
+					</Center>
+				</Paper>
+				<Paper shadow="sm" radius="md" p="md" withBorder style={{maxWidth: `300px`, height: `285px`}}>
+					<Center>
+						<Title order={3}>Скидки</Title>
+					</Center>
+				</Paper>
+			</Group>
+			<Space h="md" />
+			<Group grow>
+				<Paper shadow="sm" radius="md" p="md" withBorder style={{maxWidth: `278px`, height: `285px`}}>
+					<Center>
+						<Title order={3}>Стримеры</Title>
+					</Center>
+				</Paper>
+				<Paper shadow="sm" radius="md" p="md" withBorder style={{maxWidth: `278px`, height: `285px`}}>
+					<Center>
+						<Title order={3}>NSFW</Title>
+					</Center>
+				</Paper>
+				<Paper shadow="sm" radius="md" p="md" withBorder style={{maxWidth: `278px`, height: `285px`}}>
+					<Center>
+						<Title order={3}>Криптовалюта</Title>
+					</Center>
+				</Paper>
+			</Group>
+		</>	
 	);
 }
